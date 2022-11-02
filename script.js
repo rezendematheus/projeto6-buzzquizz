097a68037a09a6eb2f0742ceb612a80c9b6234be
pedrorosadomingues <pedrorosadomingues@gmail.com>
2022-11-02 02:37:53 -0300

    renderizando Quizz dinamicamente

---
+++ script.js
@@ -0,0 +1,49 @@
+
+function getQuizz(){
+const quizz = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/1')
+
+quizz.then(showQuizz)
+}
+
+function showQuizz(quizz){
+let header = document.querySelector(".main-header")
+let mainContent = document.querySelector(".main-content")
+
+header.innerHTML += `<div style = "background-image: url(${quizz.data.image})" class="main-header-container-bottom">
+
+<h1>${quizz.data.title}</h1>
+</div>
+`
+
+for ( i = 0; i < quizz.data.questions.length; i++){
+    mainContent.innerHTML += `<div class="quest-container">
+<div class="quest-title">
+    <h2>${quizz.data.questions[i].title}</h2>
+</div>
+<div class="options-container">
+    <div class="option">
+        <img src=${quizz.data.questions[i].answers[0].image} alt="option">
+        <p>${quizz.data.questions[i].answers[0].text}</p>
+        
+    </div>
+    <div class="option">
+        <img src=${quizz.data.questions[i].answers[1].image} alt="option">
+        <p>${quizz.data.questions[i].answers[1].text}</p>
+        
+    </div>
+    <div class="option">
+        <img src=${quizz.data.questions[i].answers[2].image} alt="option">
+        <p>${quizz.data.questions[i].answers[2].text}</p>
+        
+    </div>
+    <div class="option">
+        <img src=${quizz.data.questions[i].answers[3].image} alt="option">
+        <p>${quizz.data.questions[i].answers[3].text}</p>
+        
+    </div>
+</div>  `
+}
+    console.log(quizz)
+}
+
+getQuizz()
