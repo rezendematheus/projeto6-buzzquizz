05ee00788f1f880a0421a110f9afd3fc1fef354e
pedrorosadomingues <pedrorosadomingues@gmail.com>
2022-11-02 03:24:30 -0300

    comportamento de respostas iniciado

--- script.js
+++ script.js
@@ -21,22 +21,22 @@ for ( i = 0; i < quizz.data.questions.length; i++){
     <h2>${quizz.data.questions[i].title}</h2>
 </div>
 <div class="options-container">
-    <div class="option">
+    <div onclick="selectAnswer(this,${quizz.data.questions[i].answers[0].isCorrectAnswer} )" class="option">
         <img src=${quizz.data.questions[i].answers[0].image} alt="option">
         <p>${quizz.data.questions[i].answers[0].text}</p>
         
     </div>
-    <div class="option">
+    <div onclick="selectAnswer(this,${quizz.data.questions[i].answers[1].isCorrectAnswer})" class="option">
         <img src=${quizz.data.questions[i].answers[1].image} alt="option">
         <p>${quizz.data.questions[i].answers[1].text}</p>
         
     </div>
-    <div class="option">
+    <div onclick="selectAnswer(this,${quizz.data.questions[i].answers[2].isCorrectAnswer})" class="option">
         <img src=${quizz.data.questions[i].answers[2].image} alt="option">
         <p>${quizz.data.questions[i].answers[2].text}</p>
         
     </div>
-    <div class="option">
+    <div onclick="selectAnswer(this,${quizz.data.questions[i].answers[3].isCorrectAnswer})" class="option">
         <img src=${quizz.data.questions[i].answers[3].image} alt="option">
         <p>${quizz.data.questions[i].answers[3].text}</p>
         
@@ -46,4 +46,14 @@ for ( i = 0; i < quizz.data.questions.length; i++){
     console.log(quizz)
 }
 
+function selectAnswer(option, isCorrectAnswer){
+
+    if (isCorrectAnswer){
+    option.classList.add("correct")
+    } else {
+    option.classList.add("wrong")
+    }
+   
+  console.log(isCorrectAnswer)
+}
 getQuizz()
