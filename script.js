6de26bc45dd7d8f42e20a7e6a9d85016c759c3f1
Matheus Rezende <matheusreisrezende.2002@gmail.com>
2022-11-02 12:30:47 -0300

    Layout Tela 1 completo - Função de Listar Quizzes, falta toggle() pro layout de adicionar função

--- script.js
+++ script.js
@@ -1,4 +1,58 @@
 
+//Funções úteis
+const retornaErro = (erro) => {
+    console.log(erro)
+}
+const retornaSucesso = (sucesso) => {
+    console.log(sucesso)
+}
+
+//Tela 1
+function listarQuizz(response){
+    quizzList()
+    const list = document.querySelector(".todos-quizzes")
+    console.log(response.data)
+    response.data.forEach(element => {
+        list.querySelector(".quizz-container").innerHTML += 
+        `<div class="go-quizz" onclick="">
+            <img src="${element.image}" alt="">
+            <p>${element.title}</p>
+        </div>`
+    });
+}
+
+function quizzList () {
+    let body = document.querySelector("body")
+    body.innerHTML += 
+    `<div class="tela-1">
+        <div class="content-1">
+            <div class="criar-quizz">
+                <p>Você não criou nenhum quizz ainda :(</p>
+                <button onclick="criarQuizz()">Criar Quizz</button>
+            </div>
+            <div class="seus-quizzes not-display">
+                <p>Seus Quizzes <ion-icon onclick="criarQuizz()" name="add-circle"></ion-icon></p>
+                <div class = "quizz-container">
+
+                </div>
+            </div>
+            <div class="todos-quizzes">
+                <p>Todos os Quizzes</p>
+                <div class="quizz-container"></div>
+            </div>
+        </div>
+    </div>`
+}
+function getList() {
+    axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes")
+    .catch(retornaErro)
+    .then(listarQuizz)
+}
+
+//getList()
+
+
+//Tela 2 
 function getQuizz(){
 const quizz = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/1')
 
@@ -56,4 +110,5 @@ function selectAnswer(option, isCorrectAnswer){
    
   console.log(isCorrectAnswer)
 }
-getQuizz()
+//getQuizz()
+
